270447b30edb5f3a2647df111cc991e50117f591
konstantin koltypanov <k.koltypanov@nv.dunice.net>
2025-11-20 15:46:08 +0300

    initial repo

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {
+    // Добавьте ваши редьюсеры здесь
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
